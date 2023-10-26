3813014e6c786493cc4727b70a3d7d76890022a9
obaraelijah <elijahobara357@gmail.com>
2023-10-26 14:51:48 +0300

    chore: basic projectc setup

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,6 @@ const app = require('./app');
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server started on port http://127.0.0.1:${port}/`));
+app.listen(port, () => 
+    console.log(`Server started on port http://127.0.0.1:${port}/`)
+);
