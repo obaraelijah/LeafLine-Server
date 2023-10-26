bd7960d3844b9bd63360ae79cdb9f049e9836cb1
obaraelijah <elijahobara357@gmail.com>
2023-10-26 15:08:56 +0300

    feat: Add error and not found middleware

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { createWriteStream } = require('fs');
 const { join } = require('path');
+const { errorHandler, notFound } = require('./app/middleware/errorMiddleware');
 
 const app = express();
 
@@ -42,5 +43,9 @@ app.use(
       .status(200)
       .json({ statusCode: 200, success: true, message: 'Health OK' });
   });
+  
+  //custom middleware
+  app.use(notFound);
+  app.use(errorHandler);
 
   module.exports = app;
