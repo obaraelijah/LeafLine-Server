572de308b1e87036057bf2436e3fc2465efaa232
obaraelijah <elijahobara357@gmail.com>
2023-10-28 18:47:14 +0300

    feat: Add generate token helper

---
+++ backend/helper/generateToken.js
@@ -0,0 +1,28 @@
+// Basic Lib Imports
+const jwt = require("jsonwebtoken");
+
+/**
+ * @desc Generate JWT Token
+ * @requires User id
+ * */
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+  };
+
+/**
+ * @desc Generate reset password token and set expiry time
+ *
+ * */
+
+const generateResetToken = () => {
+    const resetPasswordToken = crypto.randomBytes(20).toString("hex");
+    const resetPasswordExpiry = Date.now() + 3600000; // Token expires in 1 hour
+  
+    return { resetPasswordToken, resetPasswordExpiry };
+  };
+  
+  module.exports = { generateToken, generateResetToken };
+  
