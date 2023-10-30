b93e5d014c57c1a6ebb4b2cc34de04f22dc4b92b
obaraelijah <elijahobara357@gmail.com>
2023-10-30 18:46:04 +0300

    feat: Add user controller

--- backend/app/module/user/user.controller.js
+++ backend/app/module/user/user.controller.js
@@ -48,3 +48,70 @@ const getMe = asyncHandler(async (req, res, next) => {
     next(error)
   }
 });
+
+/**
+ * @desc    Get all users → Only Allowed For Admin
+ * @route   /api/v1/users/
+ * @method  GET
+ * @access  Private
+ */
+
+const userList = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: "Users retrived successfully",
+      data: [users],
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      error: error.message,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+/**
+ * @desc    Get a Single User → Only Allowed For Admin
+ * @route   /api/v1/users/:id
+ * @param {String} id
+ * @method  GET
+ * @access  Private
+ */
+const findUserById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id).lean();
+
+    if (!user) {
+      return res.status(404).json({
+        status: 404,
+        error: "Not Found",
+        message: "User not found.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: "User fetched successfully",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      statusCode: 500,
+      error: "Internal Server Error",
+      message: "An error occurred while fetching the book.",
+    });
+  }
+});
+module.exports = {
+  getMe,
+  userList,
+  findUserById,
+};
