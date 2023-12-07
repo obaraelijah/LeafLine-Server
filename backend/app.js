ef679f934ff9fb554a1f3ea4293b6178593bafcf
obaraelijah <elijahobara357@gmail.com>
2023-12-07 14:39:01 +0300

    chore: refactor code

--- backend/app.js
+++ backend/app.js
@@ -8,23 +8,44 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { createWriteStream } = require('fs');
-const { join } = require('path');
 const { errorHandler, notFound } = require('./app/middleware/errorMiddleware');
 
-const app = express();
+
+const applicationRoutes = require('./app/routes/index');
 
 // Database connection with mongoose
-const connectDB = require('./config/db'); 
+const connectDB = require('./config/db');
 connectDB();
+
+const app = express();
 
 app.use(helmet());
 app.use(morgan('dev'));
 app.disable('x-powered-by');
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors('*'));
 
+// // Define an array of allowed origins
+// const allowedOrigins = [
+//   `http://localhost:5173`,
+//   `https://leafline-admin.vercel.app/`,
+//   `https://leafline.vercel.app/`,
+//   // Add more domains as needed
+// ];
+
+// // Configure CORS with the allowed origins
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Check if the origin is in the list of allowed origins or if it's undefined (for same-origin requests)
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true); // Allow the request
+//     } else {
+//       callback(new Error('Not allowed by CORS')); // Deny the request
+//     }
+//   },
+//   credentials: true,
+// }));
 
 app.use(
   morgan('common', {
@@ -33,19 +54,18 @@ app.use(
     }),
   })
 );
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use('/api/v1/', applicationRoutes);
+app.use('/', (req, res) => {
+  res
+    .status(200)
+    .json({ statusCode: 200, success: true, message: 'Health OK' });
+});
+app.use(notFound);
+app.use(errorHandler);
 
- app.use(express.urlencoded(
-      { extended: true }
-  ));
-
-  app.use('/', (req, res) => {
-    res
-      .status(200)
-      .json({ statusCode: 200, success: true, message: 'Health OK' });
-  });
-  
-  //custom middleware
-  app.use(notFound);
-  app.use(errorHandler);
-
-  module.exports = app;
+module.exports = app;
